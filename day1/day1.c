c067257c5f5717596de5fe399527ff5f8dec5dfd
Michael G. Schwern <schwern@pobox.com>
2015-12-05 23:15:29 -0800

    Solve part two of day1.

--- day1/day1.c
+++ day1/day1.c
@@ -1,24 +1,48 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/errno.h>
+#include <stdlib.h>
 
-static int read_floor_instructions(FILE *fp) {
+struct Floors {
+    int start_floor;
+    int end_floor;
+    int first_enter_basement;
+};
+
+static struct Floors *Floors_create() {
+    struct Floors *floors = malloc(sizeof(struct Floors));
+
+    floors->start_floor = 0;
+    floors->end_floor = 0;
+    floors->first_enter_basement = -1;
+
+    return floors;
+}
+    
+static struct Floors *read_floor_instructions(FILE *fp) {
     char c;
-    int floor = 0;
+    int position = 0;
+    struct Floors *floors = Floors_create();
     
     while( !feof(fp) ) {
         c = (char)fgetc(fp);
         switch(c) {
             case '(':
-                floor++;
+                position++;
+                floors->end_floor++;
                 break;
             case ')':
-                floor--;
+                position++;
+                floors->end_floor--;
                 break;
+        }
+
+        if( floors->first_enter_basement < 0 && floors->end_floor < 0 ) {
+            floors->first_enter_basement = position;
         }
     }
 
-    return floor;
+    return floors;
 }
 
 int main(int argc, char **argv) {
@@ -35,8 +59,11 @@ int main(int argc, char **argv) {
         return errno;
     }
     
-    int floor = read_floor_instructions(floor_fp);
-    printf("The instructions take Santa to floor %d.\n", floor);
+    struct Floors *floors = read_floor_instructions(floor_fp);
+    printf("The instructions take Santa to floor %d.\n", floors->end_floor);
+    if( floors->first_enter_basement > 0 ) {
+        printf("Santa enters the basement at position %d.\n", floors->first_enter_basement);
+    }
 
     return 0;
 }
