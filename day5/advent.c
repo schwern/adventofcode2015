81cac1ff8ea8238b31108fee4db2dd271ec8dfec
Michael G. Schwern <schwern@pobox.com>
2015-12-07 02:10:38 -0800

    Shave a bit of time off by compiling the regexes.
    Saves about 5-10% on large inputs.

--- day5/advent.c
+++ day5/advent.c
@@ -5,16 +5,25 @@
 #include <stdlib.h>
 #include <glib.h>
 
+GError *ReError;
+GRegex *ReRepeat;
+GRegex *RePair;
+
+static void init_regexes() {
+    RePair = g_regex_new("(.).\\1", G_REGEX_OPTIMIZE, 0, &ReError);
+    ReRepeat = g_regex_new("((.)[^\\2]).*\\1", G_REGEX_OPTIMIZE, 0, &ReError);
+}
+
 static bool is_nice(char *string) {
     /* It contains at least one letter which repeats with exactly one letter
        between them, like xyx, abcdefeghi (efe), or even aaa. */
-    if( !g_regex_match_simple("(.).\\1", string, 0, 0) )
+    if( !g_regex_match(ReRepeat, string, 0, NULL ) )
         return false;
 
     /* It contains a pair of any two letters that appears at least twice in
        the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but
        not like aaa (aa, but it overlaps). */
-    if( !g_regex_match_simple("((.)[^\\2]).*\\1", string, 0, 0) )
+    if( !g_regex_match(RePair, string, 0, NULL ) )
         return false;
     
     return true;
@@ -43,6 +52,8 @@ int main(const int argc, char **argv) {
 
     FILE *fp = open_file(argv[1], "r");
 
+    init_regexes();
+    
     int num_nice = count_nice(fp);
     printf("%d\n", num_nice);
 
