071bfc41c4e4dd4326e0c7fa8b1bf1e74ab2444f
Michael G. Schwern <schwern@pobox.com>
2015-12-13 00:24:47 -0800

    Day 7: Normalize "gate" terminology. Set gate inputs.
    read_gate_line() now returns the gate and its inputs.  Then outside code
    matches up the inputs to existing gates (or makes new ones).  Null
    gates are properly handled.

--- day7/advent.c
+++ day7/advent.c
@@ -8,7 +8,7 @@
 #include <stdint.h>
 #include "gate.h"
 
-GRegex *Circuit_Line_Re;
+GRegex *Gate_Line_Re;
 GRegex *Blank_Line_Re;
 
 static void init_regexes() {
@@ -20,8 +20,8 @@ static void init_regexes() {
         );
     }
     
-    if( !Circuit_Line_Re ) {
-        Circuit_Line_Re = compile_regex(
+    if( !Gate_Line_Re ) {
+        Gate_Line_Re = compile_regex(
             " ^ \\s* "
             " (?: "
             "   (?:(?:(?<LEFT>[[:alnum:]]+) \\s+ )? (?<OP>[[:alpha:]]+) \\s+ )? (?<RIGHT>[[:alnum:]]+) "
@@ -36,7 +36,7 @@ static void init_regexes() {
 }
 
 static void free_regexes() {
-    g_regex_unref(Circuit_Line_Re);
+    g_regex_unref(Gate_Line_Re);
 }
 
 static gint cmp_keys(gconstpointer _a, gconstpointer _b) {
@@ -46,17 +46,21 @@ static gint cmp_keys(gconstpointer _a, gconstpointer _b) {
     return strcmp(a, b);
 }
 
-static void print_state_cb(gpointer _key, gpointer _val, gpointer user_data) {
-    char *key    = (char *)_key;
-    GateVal val = *(GateVal *)_val;
+static void print_gate_cb(gpointer key, gpointer val, gpointer user_data) {
+    char *name = (char *)key;
+    Gate *gate = (Gate *)val;
+    GateOp *op = gate->proto->op;
 
-    printf("%s: %d\n", key, val);
+    printf("Gate %s/%s %s", name, gate->name, op->name);
+    if( op->type != UNDEF )
+        printf(" = %d", __(gate, get));
+    puts("");
 }
 
-static void state_foreach_sorted(GHashTable *state, GHFunc cb) {
-    GList *sorted_keys = g_list_sort(g_hash_table_get_keys( state ), cmp_keys);
+static void gates_foreach_sorted(GHashTable *gates, GHFunc cb) {
+    GList *sorted_keys = g_list_sort(g_hash_table_get_keys( gates ), cmp_keys);
     for (GList *k = sorted_keys; k != NULL; k = k->next) {
-        cb( k->data, g_hash_table_lookup( state, k->data ), NULL );
+        cb( k->data, g_hash_table_lookup( gates, k->data ), NULL );
     }
 }
 
@@ -83,54 +87,99 @@ static bool is_number(char *key) {
     return true;
 }
 
-static void process_circuit_line(GHashTable *state, char *line) {
+static Gate *read_gate_line(char *line, char **inputs) {
     GMatchInfo *match;
 
     if( g_regex_match(Blank_Line_Re, line, 0, NULL) ) {
         if( DEBUG )
             fprintf(stderr, "Blank line.\n");
-        return;
+        return NULL;
     }
     
-    if( !g_regex_match(Circuit_Line_Re, line, 0, &match) ) {
+    if( !g_regex_match(Gate_Line_Re, line, 0, &match) ) {
         fprintf(stderr, "Cannot understand %s.\n", line);
-        return;
+        return NULL;
     }
     
     char *name   = get_match(match, "NAME");
     char *opname = get_match(match, "OP");
     char *right  = get_match(match, "RIGHT");
-    if( !opname || opname[0] == '\0' ) {
-        if( is_number(right) )
-            opname = "CONST";
-        else
-            opname= "SET";
+    char *left   = get_match(match, "LEFT");
+    if( is_empty(opname) ) {
+        opname= "SET";
     }
     
-    if( DEBUG )
-        fprintf(stderr, "Op: '%s'.\n", opname );
-
     Gate *gate = Gate_factory(Op_lookup(opname), name);
 
-    printf("Gate %s with op %s\n", gate->name, gate->proto->op->name);
+    /* For "OP right", right is the first input */
+    if( is_empty(left) ) {
+        inputs[0] = strdup(right);
+    }
+    /* For "left OP right", left is the first input */
+    else {
+        inputs[0] = strdup(left);
+        inputs[1] = strdup(right);
+    }
     
     g_match_info_free(match);
+
+    return gate;
+}
+
+static void set_gate_inputs(GHashTable *gates, Gate *gate, char **inputs) {
+    GateOpType optype = gate->proto->op->type;
+
+    /* Turn 123 -> a into a CONST op */
+    if( optype == SET && is_number(inputs[0])) {
+        __(gate, set_op, &Op_Const);
+        __(gate, set_value, atoi(inputs[0]));
+        return;
+    }
+        
+    for( int i = 0; i < gate->proto->op->num_inputs; i++ ) {
+        char *input_str = inputs[i];
+        Gate *input_gate;
+
+        if( !(input_gate = g_hash_table_lookup(gates, input_str)) ) {
+            if( is_number(input_str) ) {
+                input_gate = Gate_factory(&Op_Const, input_str);
+                __(input_gate, set_value, atoi(input_str));
+            }
+            else {
+                input_gate = Gate_factory(&Op_Undef, input_str);
+            }
+
+            g_hash_table_insert(gates, input_str, input_gate);
+        }
+
+        __(gate, set_input, i, input_gate);
+    }
 }
 
 static GHashTable *read_circuit(FILE *fp) {
     char *line = NULL;
     size_t line_size = 0;
-    GHashTable *state = g_hash_table_new(g_str_hash, g_str_equal);
+    char **inputs = calloc(2, sizeof(char));
+    GHashTable *gates = g_hash_table_new(g_str_hash, g_str_equal);
 
     init_regexes();
     
     while( getline(&line, &line_size, fp) > 0 ) {
-        process_circuit_line(state, line);
+        Gate *gate = read_gate_line(line, inputs);
+        if( !gate ) {
+            printf("Unknown line: %s", line);
+        }
+        else {
+            g_hash_table_insert(gates, gate->name, gate);
+            set_gate_inputs(gates, gate, inputs);
+        }
     }
-
+    free(line);
+    free(inputs);
+    
     free_regexes();
     
-    return state;
+    return gates;
 }
 
 int main(const int argc, char **argv) {
@@ -144,9 +193,10 @@ int main(const int argc, char **argv) {
     if( argc == 2 )
         input = open_file(argv[1], "r");
 
-    GHashTable *state = read_circuit(input);
-    state_foreach_sorted(state, print_state_cb);
-    g_hash_table_unref(state);
+    GHashTable *gates = read_circuit(input);
+    gates_foreach_sorted(gates, print_gate_cb);
+
+    g_hash_table_unref(gates);
 
     return 0;
 }
