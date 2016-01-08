42ac929cd19d69772703590c436573ed9defcd9b
Michael G. Schwern <schwern@pobox.com>
2016-01-07 19:22:37 -0800

    Racing reindeer works.

--- day14/advent.c
+++ day14/advent.c
@@ -23,6 +23,25 @@ static void Reindeer_destroy(Reindeer *self) {
     free(self);
 }
 
+static int Reindeer_travel(Reindeer *self, int time) {
+    int cycle = self->flight_time + self->rest_time;
+
+    assert( time > 0 );
+    
+    time = time % cycle;
+
+    /* If the time is right at the end of the rest cycle */
+    if( time == 0 ) {
+        return 0;
+    }
+    else if( time <= self->flight_time ) {
+        return self->flight_speed;
+    }
+    else {
+        return 0;
+    }
+}
+
 static void Reindeer_print(Reindeer *self) {
     printf("name: %s, flight_speed: %d, flight_time: %d, rest_time: %d\n",
            self->name, self->flight_speed, self->flight_time, self->rest_time
@@ -82,24 +101,46 @@ static void read_reindeer_line( char *line, Reindeer **reindeer_p ) {
     return;
 }
 
-#define NUM_TEST_REINDEER 2
-static void test_reindeer() {
-    char *lines[NUM_TEST_REINDEER] = {
-        "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.\n",
-        "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.\n"
-    };
+static int max_of(int things[], int num_things) {
+    int max = -INT_MAX;
+    for( int i = 0; i < num_things; i++ ) {
+        max = MAX( max, things[i] );
+    }
 
-    init_regexes();
+    return max;
+}
+
+static int race_reindeer(Reindeer **reindeers, int num_reindeer, int race_length) {
+    int distances[num_reindeer];
+    for( int i = 0; i < num_reindeer; i++ ) {
+        distances[i] = 0;
+    }
+    
+    for( int time = 1; time <= race_length; time++ ) {
+        for( int j = 0; j < num_reindeer; j++ ) {
+            distances[j] += Reindeer_travel(reindeers[j], time);
+        }
+    }
+
+    return max_of(distances, num_reindeer);
+}
+
+#define NUM_TEST_REINDEER 2
+char *Test_Lines[NUM_TEST_REINDEER] = {
+    "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.\n",
+    "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.\n"
+};
+
+static void test_reindeer() {
+    printf("test_reindeer\n");
     
     Reindeer *reindeers[NUM_TEST_REINDEER];
     for( int i = 0; i < NUM_TEST_REINDEER; i++ ) {
         Reindeer *reindeer = NULL;
-        read_reindeer_line( lines[i], &reindeer );
+        read_reindeer_line( Test_Lines[i], &reindeer );
 
         reindeers[i] = reindeer;
     }
-
-    free_regexes();
 
     assert( streq(reindeers[0]->name, "Comet") );
     assert( reindeers[0]->flight_speed == 14 );
@@ -111,6 +152,29 @@ static void test_reindeer() {
     assert( reindeers[1]->flight_time  == 11 );
     assert( reindeers[1]->rest_time    == 162 );
 
+    assert( Reindeer_travel(reindeers[1], 1) == 16 );
+    assert( Reindeer_travel(reindeers[1], 11) == 16 );
+    assert( Reindeer_travel(reindeers[1], 12) == 0 );
+    assert( Reindeer_travel(reindeers[1], 11 + 162) == 0 );
+    assert( Reindeer_travel(reindeers[1], 11 + 162 + 1) == 16 );
+
+    for( int i = 0; i < NUM_TEST_REINDEER; i++ ) {
+        Reindeer_destroy(reindeers[i]);
+    }
+}
+
+static void test_race_reindeer() {
+    printf("test_race_reindeer\n");
+    
+    Reindeer *reindeers[NUM_TEST_REINDEER];
+    for( int i = 0; i < NUM_TEST_REINDEER; i++ ) {
+        Reindeer *reindeer = NULL;
+        read_reindeer_line( Test_Lines[i], &reindeer );
+        reindeers[i] = reindeer;
+    }
+
+    assert( race_reindeer(reindeers, NUM_TEST_REINDEER, 1000) == 1120 );
+
     for( int i = 0; i < NUM_TEST_REINDEER; i++ ) {
         Reindeer_destroy(reindeers[i]);
     }
@@ -118,7 +182,14 @@ static void test_reindeer() {
 
 static void run_tests() {
     printf("Running tests\n");
+
+    init_regexes();
+    
     test_reindeer();
+    test_race_reindeer();
+
+    free_regexes();
+    
     printf("OK\n");
 }
 
